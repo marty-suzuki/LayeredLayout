8229ecd32caa28c313720a14828f521a646c8195
marty-suzuki <s1180183@gmail.com>
2020-02-05 03:33:01 +0900

    Add project

---
+++ Sources/LayeredLayout/LayeredLayout.swift
@@ -0,0 +1,3 @@
+struct LayeredLayout {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import LayeredLayoutTests
+
+var tests = [XCTestCaseEntry]()
+tests += LayeredLayoutTests.allTests()
+XCTMain(tests)
