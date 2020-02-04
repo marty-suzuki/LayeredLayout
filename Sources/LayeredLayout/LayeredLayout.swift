1ab074297d62fe2cfe9569a9e01561e9a86be35b
marty-suzuki <s1180183@gmail.com>
2020-02-05 03:35:05 +0900

    Add LayeredLayoutExtension

--- Sources/LayeredLayout/LayeredLayout.swift
+++ Sources/LayeredLayout/LayeredLayout.swift
@@ -1,3 +1,16 @@
-struct LayeredLayout {
-    var text = "Hello, World!"
+public protocol LayeredLayoutCompatible {
+    associatedtype LayeredLayoutBase
+    var layout: LayeredLayoutExtension<LayeredLayoutBase> { get }
+}
+
+extension LayeredLayoutCompatible {
+    public var layout: LayeredLayoutExtension<Self> { .init(self) }
+}
+
+public struct LayeredLayoutExtension<Base> {
+    let base: Base
+
+    fileprivate init(_ base: Base) {
+        self.base = base
+    }
 }
